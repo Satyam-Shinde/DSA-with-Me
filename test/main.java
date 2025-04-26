4280ed040f04ec3ea07f2f1aa6ba7c83f0b017f7
Atharv <lokhandeatharva225@gmail.com>
2025-04-26 22:35:04 +0530

    test

---
+++ test/main.java
@@ -0,0 +1,11 @@
+public class main{
+    public static void main(String[] args) {
+    
+        System.out.println("Hello, World!");
+        System.out.println("This is a test.");
+        System.out.println("Goodbye, World!");
+        System.out.println("This is the end.");
+        System.out.println("End of the test.");
+        System.out.println("Final line.");
+    }
+}
