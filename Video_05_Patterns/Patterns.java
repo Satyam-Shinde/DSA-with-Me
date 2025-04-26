5c46b08ba41faa6dd2da154e4bce15851f8af258
Satyam-Shinde <satyamshinde2004@gmail.com>
2025-04-26 22:09:29 +0530

    Added Video_06 file and modified the Video_05 file

--- Video_05_Patterns/Patterns.java
+++ Video_05_Patterns/Patterns.java
@@ -3,91 +3,86 @@ package Video_05_Patterns;
 public class Patterns {
     public static void main(String[] args){
         int n = 5;
-        int m = 5;
-//        Problem 1: Solid Rectangle
-//        for(int i=1; i<=n; i++){
-//            for(int j=1; j<=m; j++){
-//                System.out.print("*");
-//            }
-//            System.out.println("*");
-//        }
+        int m = 4;
+/*
+        Problem 1: Solid Rectangle
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+        Problem 2: Hollow Rectangle
+        for (int i = 1; i <= n; i++) {
+            for( int j = 1; j <= m; j++){
+                if(i == 1 || j == 1 || i == n || j == m){
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        Problem 3 : Half Pyramid
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        Problem 4 : Inverted Half Pyramid
+        for(int i = n; i >= 1; i--){
+            for (int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        Problem 5 : Inverted Half Pyramid (rotated by 180 deg)
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n-i; j++){
+                System.out.print(" ");
+            }
 
-//        Problem 2: Hollow Rectangle
-//        for (int i = 1; i <= n; i++) {
-//            for( int j = 1; j <= m; j++){
-//                if(i == 1 || j == 1 || i == n || j == m){
-//                    System.out.print("*");
-//                } else{
-//                    System.out.print(" ");
-//                }
-//            }
-//            System.out.println();
-//        }
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        Problem 6 : Half Pyramid with numbers
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        Problem 7 : Inverted half Pyramid with numbers
+        for (int i = n; i>= 1; i--){
+            for(int j = 1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        Problem 8 : Floyd's Triangle
+        int k = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(k + " ");
+                k++
+            }
+            System.out.println();
+        }
+        Problem 9 : 0 - 1 Triangle
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                if((i+j) % 2 == 0){
+                    System.out.print(1+" ");
+                } else {
+                    System.out.print(0+" ");
+                }
+            }
+            System.out.println();
+        }
+*/
 
-//        Problem 3 : Half Pyramid
-//        for (int i = 1; i <= n; i++) {
-//            for (int j = 1; j <= i; j++){
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-
-//        Problem 4 : Inverted Half Pyramid
-//        for(int i = n; i >= 1; i--){
-//            for (int j = 1; j<=i; j++){
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-
-//        Problem 5 : Inverted Half Pyramid (rotated by 180 deg)
-//        for(int i = 1; i <= n; i++){
-//            for(int j = 1; j <= n-i; j++){
-//                System.out.print(" ");
-//            }
-//
-//            for(int j = 1; j<=i; j++){
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-
-//        Problem 6 : Half Pyramid with numbers
-//        for(int i = 1; i <= n; i++){
-//            for(int j = 1; j <= i; j++){
-//                System.out.print(j+" ");
-//            }
-//            System.out.println();
-//        }
-
-//        Problem 7 : Inverted half Pyramid with numbers
-//        for (int i = n; i>= 1; i--){
-//            for(int j = 1; j<=i; j++){
-//                System.out.print(j+" ");
-//            }
-//            System.out.println();
-//        }
-
-//        Problem 8 : Floyd's Triangle
-//        int k = 1;
-//        for(int i = 1; i <= n; i++){
-//            for(int j = 1; j <= i; j++){
-//                System.out.print(k + " ");
-//                k++
-//            }
-//            System.out.println();
-//        }
-
-//        Problem 9 : 0 - 1 Triangle
-//        for(int i = 1; i <= n; i++){
-//            for(int j = 1; j <= i; j++){
-//                if((i+j) % 2 == 0){
-//                    System.out.print(1+" ");
-//                } else {
-//                    System.out.print(0+" ");
-//                }
-//            }
-//            System.out.println();
-//        }
     }
 }
---
+++ Video_06_AdvPatterns/AdvancedPatterns.java
@@ -0,0 +1,94 @@
+package Video_06_AdvPatterns;
+
+public class AdvancedPatterns {
+    public static void main(String[] args){
+        int n = 4;
+//        Pattern 10: Butterfly Pattern
+//        for(int i = 1; i <= n; i++){
+//            for(int j = 1; j <= i; j++){
+//                System.out.print("*");
+//            }
+//            int spaces = 2*(n-i);
+//            for(int j = 1; j<=spaces; j++){
+//                System.out.print(" ");
+//            }
+//
+//            for(int j = 1; j <= i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i = n; i >= 1; i--){
+//            for(int j = 1; j <= i; j++){
+//                System.out.print("*");
+//            }
+//            int spaces = 2*(n-i);
+//            for(int j = 1; j<=spaces; j++){
+//                System.out.print(" ");
+//            }
+//
+//            for(int j = 1; j <= i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+//        Problem 11: Solid Rhombus
+//        for(int i = 1; i <= n; i++){
+//            for(int j = 1; j <= (n-i); j++){
+//                System.out.print(" ");
+//            }
+//            for(int j = 1; j <= 5; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        Problem 12: Number Pyramid
+//        for(int i = 1; i<=n; i++){
+//            for(int j = 1; j <= n-i; j++){
+//                System.out.print(" ");
+//            }
+//            for(int j = 1; j<=i; j++){
+//                System.out.print(i + " ");
+//            }
+//            System.out.println();
+//        }
+
+//        Problem 13: Palindromic Pyramid
+//        for(int i = 1; i <= n; i++){
+//            for(int j = 1; j <= n-i; j++){
+//                System.out.print(" ");
+//            }
+//
+//            for(int j = i; j>=1; j--){
+//                System.out.print(j);
+//            }
+//            for (int j = 2; j <= i; j++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+
+//        Problem 14: Diamond
+//        for(int i = 1; i <= n; i++){
+//            for(int j = 1; j <= n-i; j++){
+//                System.out.print(" ");
+//            }
+//
+//            for(int j = 1; j<=2*i-1; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i = n; i >= 1; i--){
+//            for(int j = 1; j <= n-i; j++){
+//                System.out.print(" ");
+//            }
+//
+//            for(int j = 1; j<=2*i-1; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+    }
+}
