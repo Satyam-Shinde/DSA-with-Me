8fdcc0e270e1bd964b501b526f52cb0b56bac602
atharv <lokhandeatharva225@gmail.com>
2025-05-12 09:06:27 +0530

    user input addition program

---
+++ Video_02/add.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter The First number: ");
+        int a = sc.nextInt();
+
+        System.out.println("Enter the Second Number: ");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("The Sum Is:"+ sum);
+
+
+
+}
+    
+}
